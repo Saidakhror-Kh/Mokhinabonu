8c9b3bd84ebc3e567a81a058dc2560b3f6f33be6
Saidakhror-Kh <xojakbarovsaidaxror@gmail.com>
2020-12-24 06:05:19 +0500

    end

---
+++ js/script.js
@@ -0,0 +1,65 @@
+
+var butn1=document.querySelector(".but");
+var butn2=document.querySelector(".but2");
+var butn3=document.querySelector(".but3");
+var butn4=document.querySelector(".but4");
+var butn5=document.querySelector(".but5");
+var butn6=document.querySelector(".but6");
+
+i=0
+    butn1.onclick = function(){
+        if (i==0) {
+            alert('Поздравляю вы выиграли коробку сладостей!');
+            i++
+        }
+        else{
+            alert('Вы уже получили свой подарок')
+        }
+    }
+    butn2.onclick = function(){
+        if (i==0) {
+            alert('Поздравляю вы выиграли коробку сладостей!');
+            i++
+        }
+        else{
+            alert('Вы уже получили свой подарок')
+        }
+    }
+    butn3.onclick = function(){
+        if (i==0) {
+            alert('Поздравляю вы выиграли коробку сладостей!');
+            i++
+        }
+        else{
+            alert('Вы уже получили свой подарок')
+        }
+    }
+    butn4.onclick = function(){
+        if (i==0) {
+            alert('Поздравляю вы выиграли коробку сладостей!');
+            i++
+        }
+        else{
+            alert('Вы уже получили свой подарок')
+        }
+    }
+    butn5.onclick = function(){
+        if (i==0) {
+            alert('Поздравляю вы выиграли коробку сладостей!');
+            i++
+        }
+        else{
+            alert('Вы уже получили свой подарок')
+        }
+    }
+    butn6.onclick = function(){
+        if (i==0) {
+            alert('Поздравляю вы выиграли коробку сладостей!');
+            i++
+        }
+        else{
+            alert('Вы уже получили свой подарок')
+        }
+    }
+
+
